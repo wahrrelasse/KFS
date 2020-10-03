63199ca465960734b34fe4f9015e800481c326f1
Lasse <lasse.heckelmann@uni-oldenburg.de>
2020-10-03 19:42:53 +0200

    Added all fxml & Documentation

--- src/main/java/de/kfs/db/controller/AdvancedAddViewPresenter.java
+++ src/main/java/de/kfs/db/controller/AdvancedAddViewPresenter.java
@@ -1,4 +1,12 @@
 package de.kfs.db.controller;
 
+import javafx.event.ActionEvent;
+
 public class AdvancedAddViewPresenter {
+    /**
+     * Handles the addition of an E-Bike to the datastructure
+     * @param actionEvent
+     */
+    public void onAdvanceAddButtonPressed(ActionEvent actionEvent) {
+    }
 }
