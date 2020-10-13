eaebabf55ea093b0ec3b64bf3c94edec00030d69
Lasse <lasse.heckelmann@uni-oldenburg.de>
2020-10-13 12:34:09 +0200

    First half of edits to fxml, revamped editView

--- src/main/java/de/kfs/db/controller/EditViewPresenter.java
+++ src/main/java/de/kfs/db/controller/EditViewPresenter.java
@@ -2,14 +2,27 @@ package de.kfs.db.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
 
 public class EditViewPresenter extends AbstractPresenter{
 
 
     public static String fxml = "/fxml/editView.fxml";
     @FXML
-    public Button confirmEditButton;
+    public TextField numberField;
+    @FXML
+    public TextField newKeyField;
+    @FXML
+    public TextField newBpKeyField;
+    @FXML
+    public TextField frameHField;
+    @FXML
+    public TextField tireDField;
+    @FXML
+    public TextField manufacturerField;
+    @FXML
+    public TextField colorField;
 
     /**
      * Handles the editing of a bike Object with new Data gathered from textfields
