5452bf121ae98dc4991c5cd1d1c3d842019c94a2
Lasse <lasse.heckelmann@uni-oldenburg.de>
2020-10-13 12:37:14 +0200

    Added superclass to errorViewPresenter & rest of small fxml edits

--- src/main/java/de/kfs/db/controller/ErrorViewPresenter.java
+++ src/main/java/de/kfs/db/controller/ErrorViewPresenter.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
-public class ErrorViewPresenter {
+public class ErrorViewPresenter extends AbstractPresenter {
 
 
     @FXML
