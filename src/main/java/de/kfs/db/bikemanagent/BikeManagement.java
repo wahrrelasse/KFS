5ac5de434a6154a3f8f7e38f621b531f4538380c
Lasse <lasse.heckelmann@uni-oldenburg.de>
2020-10-13 16:55:13 +0200

    Added saveMethod, PredicateManagement to BikeManagement

--- src/main/java/de/kfs/db/bikemanagent/BikeManagement.java
+++ src/main/java/de/kfs/db/bikemanagent/BikeManagement.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class BikeManagement {
 
@@ -40,5 +41,36 @@ public class BikeManagement {
         } else {
             setInitialBikes(new ArrayList<>());
         }
+    }
+
+    /**
+     * opens a new FileChooser and saves the current bikes
+     * to that file
+     */
+    public void saveBikes() {
+        final FileChooser fc = new FileChooser();
+
+        File file = fc.showSaveDialog(new Stage());
+        if(file != null) AbstractBike.save(file, flBike); else {
+            //TODO errorHandling
+        }
+    }
+
+    /**
+     * Method to change the Predicate of the filteredList
+     * effectively overrides current prediacte
+     * @param p the Predicate to be applied
+     */
+    public void changePredicate(Predicate p) {
+        flBike.setPredicate(p);
+    }
+
+    /**
+     * Method to add a predicate to already existing predicate
+     * (logically AND parameter and current predicate)
+     * @param p the predicate to add
+     */
+    public void addPredicate(Predicate p) {
+        flBike.setPredicate(flBike.getPredicate().and(p));
     }
 }
